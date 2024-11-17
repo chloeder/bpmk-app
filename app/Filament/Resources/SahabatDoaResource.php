a5985dfd7deabd2a19d37566a64c220e32af1463
Steward Lumowa <stewardyohanes29@gmail.com>
2024-11-17 19:50:19 +0700

    add sahabat doa

--- app/Filament/Resources/SahabatDoaResource.php
+++ app/Filament/Resources/SahabatDoaResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -18,12 +19,25 @@ class SahabatDoaResource extends Resource
     protected static ?string $model = SahabatDoa::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Penginjilan';
+    protected static ?int $navigationSort = 4;
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('nama')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('jurusan')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('no_hp')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('pokok_doa')
+                    ->required()
+                    ->maxLength(255),
             ]);
     }
 
@@ -31,13 +45,23 @@ class SahabatDoaResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('jurusan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('no_hp')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('pokok_doa')
+                    ->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
